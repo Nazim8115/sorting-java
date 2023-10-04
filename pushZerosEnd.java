6a3b2d86468ac5c73d1985e42410d739dc2fc931
Nazim8115 <nazim.nn459@gmail.com>
2023-10-04 23:54:12 +0530

    add another solution of push zeros

--- pushZerosEnd.java
+++ pushZerosEnd.java
@@ -13,9 +13,26 @@ public class pushZerosEnd {
         }
     }
 
+    public static void solution2(int arr[]) {
+        int k = 0;
+        int n = arr.length;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                arr[k] = arr[i];
+                k++;
+
+            }
+        }
+
+        while (k < n) {
+            arr[k] = 0;
+            k++;
+        }
+    }
+
     public static void main(String[] args) {
         int arr[] = { 2, 3, 0, 4, 3, 0 };
-        solution(arr);
+        solution2(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
